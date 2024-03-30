f616183ef0180f34bd90968630b9edc51aa4df73
Daf <afahd.ali@gmail.com>
2024-03-30 03:34:18 +0100

    added loop to generate multiple passphrase

--- DafWarden.Application/Executor.cs
+++ DafWarden.Application/Executor.cs
@@ -8,19 +8,32 @@ public class Executor(IPassphraseGenerator passphraseGenerator)
 
     public void Execute()
     {
-        Console.WriteLine("Enter passphrase length as a number.");
-        var passphraseLengthString = Console.ReadLine();
-        while (!int.TryParse(passphraseLengthString, out _))
+        string? continueGeneration;
+        do
         {
-            Console.WriteLine("This is not a number, please type a number for your passwordLength.");
-            passphraseLengthString = Console.ReadLine();
-        }
+            Console.WriteLine("Enter passphrase length as a number.");
+            var passphraseLengthString = Console.ReadLine();
+            while (!int.TryParse(passphraseLengthString, out _))
+            {
+                Console.WriteLine("This is not a number, please type a number for your passwordLength.");
+                passphraseLengthString = Console.ReadLine();
+            }
 
-        int passphraseLength  = int.Parse(passphraseLengthString);
-        var passwordPhraseResult = _passphraseGenerator.Generate(passphraseLength);
-        if (passwordPhraseResult.IsSuccess)
-            Console.WriteLine(passwordPhraseResult.Value);
-        else
-            Console.WriteLine("Sorry there was an issue, please try again.");
+            int passphraseLength = int.Parse(passphraseLengthString);
+            var passwordPhraseResult = _passphraseGenerator.Generate(passphraseLength);
+            if (passwordPhraseResult.IsSuccess)
+                Console.WriteLine(passwordPhraseResult.Value);
+            else
+                Console.WriteLine("Sorry there was an issue, please try again.");
+
+            do
+            {
+                Console.WriteLine("Do you want to continue (y/n)");
+                continueGeneration = Console.ReadLine();
+            }
+            while (continueGeneration is not "y" and not "Y" and not "n" and not "n");
+        } while (continueGeneration is "y" or "Y");
+
+
     }
 }
